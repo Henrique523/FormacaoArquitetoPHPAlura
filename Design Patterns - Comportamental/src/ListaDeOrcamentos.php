60f26d66b9774dcfe12938a642b6689c9502ea3a
GUILHERME\Henrique <guilherme.hlg.eng@gmail.com>
2021-05-23 10:32:30 -0300

    finaliza curdo de Design Patterns - Comportamental

---
+++ Design Patterns - Comportamental/src/ListaDeOrcamentos.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+class ListaDeOrcamentos implements \IteratorAggregate
+{
+  /** @var Orcamento[] */
+  private array $orcamentos = [];
+
+  public function addOrcamento(Orcamento $orcamento)
+  {
+    $this->orcamentos[] = $orcamento;
+  }
+
+  public function getIterator()
+  {
+    return new \ArrayIterator($this->orcamentos);
+  }
+}
