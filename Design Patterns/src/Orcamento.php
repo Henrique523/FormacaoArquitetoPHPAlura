7ddfda98d0e188ffa9c453d82fdaa89c9cf7a99e
GUILHERME\Henrique <guilherme.hlg.eng@gmail.com>
2021-05-24 08:58:21 -0300

    finaliza padrao composite

--- Design Patterns/src/Orcamento.php
+++ Design Patterns/src/Orcamento.php
@@ -5,32 +5,48 @@ namespace Alura\DesignPattern;
 use Alura\DesignPattern\EstadosOrcamento\EmAprovacao;
 use Alura\DesignPattern\EstadosOrcamento\EstadoOrcamento;
 
-class Orcamento
+class Orcamento implements Orcavel
 {
-  public float $valor = 0.0;
-  public int $quantidadeItens = 0;
-  public EstadoOrcamento $estadoAtual;
+    private array $itens = [];
+    public EstadoOrcamento $estadoAtual;
 
-  public function __construct()
-  {
-    $this->estadoAtual = new EmAprovacao();
-  }
+    public function __construct()
+    {
+      $this->estadoAtual = new EmAprovacao();
+      $this->itens = [];
+    }
 
-  public function aplicaDescontoExtra()
-  {
-    $this->valor = $this->estadoAtual->calculaDescontoExtra($this);
-  }
+    public function aplicaDescontoExtra()
+    {
+      $this->valor = $this->estadoAtual->calculaDescontoExtra($this);
+    }
 
-  public function aprova()
-  {
-    $this->estadoAtual->aprova($this);
-  }
-  public function reprova()
-  {
-    $this->estadoAtual->reprova($this);
-  }
-  public function finaliza()
-  {
-    $this->estadoAtual->finaliza($this);
-  }
+    public function aprova()
+    {
+      $this->estadoAtual->aprova($this);
+    }
+
+    public function reprova()
+    {
+      $this->estadoAtual->reprova($this);
+    }
+
+    public function finaliza()
+    {
+      $this->estadoAtual->finaliza($this);
+    }
+
+    public function addItem(Orcavel $item)
+    {
+        $this->itens[] = $item;
+    }
+
+    public function valor(): float
+    {
+        return array_reduce(
+            $this->itens,
+            fn (float $valorAcumulado, Orcavel $item) => $item->valor() + $valorAcumulado,
+            0
+        );
+    }
 }
