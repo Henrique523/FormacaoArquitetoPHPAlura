ea4790c5d22adc2a3030ac3938d8f6d8d84118a6
GUILHERME\Henrique <guilherme.hlg.eng@gmail.com>
2021-05-24 08:38:24 -0300

    finaliza o padrao decorator

--- Design Patterns/testes.php
+++ Design Patterns/testes.php
@@ -2,23 +2,23 @@
 
 use Alura\DesignPattern\CalculadoraDeDescontos;
 use Alura\DesignPattern\CalculadoraDeImpostos;
-use Alura\DesignPattern\Impostos\Iss;
+use Alura\DesignPattern\Impostos\{Iss, Icms};
 use Alura\DesignPattern\Orcamento;
 
 require 'vendor/autoload.php';
 
-// $calculadora = new CalculadoraDeImpostos();
+ $calculadora = new CalculadoraDeImpostos();
 
-// $orcamento = new Orcamento();
+ $orcamento = new Orcamento();
 
-// $orcamento->valor = 100;
+ $orcamento->valor = 100;
 
-// echo $calculadora->calcula($orcamento, new Iss());
+ echo $calculadora->calcula($orcamento, new Iss(new Icms()));
 
-$calculadora = new CalculadoraDeDescontos();
-$orcamento = new Orcamento();
-
-$orcamento->valor = 600;
-$orcamento->quantidadeItens = 7;
-
-echo $calculadora->calculaDescontos($orcamento);
+//$calculadora = new CalculadoraDeDescontos();
+//$orcamento = new Orcamento();
+//
+//$orcamento->valor = 600;
+//$orcamento->quantidadeItens = 7;
+//
+//echo $calculadora->calculaDescontos($orcamento);
