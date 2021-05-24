f5d2bd0b7ace2da4e5eae4f009d7244cad4570eb
GUILHERME\Henrique <guilherme.hlg.eng@gmail.com>
2021-05-24 08:18:29 -0300

    implementa o padrao bridge

---
+++ Design Patterns/relatorio.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Alura\DesignPattern\{Orcamento, Pedido};
+use Alura\DesignPattern\Relatorio\{OrcamentoExportado, PedidoExportado};
+use Alura\DesignPattern\Relatorio\{ArquivoXmlExportado, ArquivoZipExportado};
+
+$orcamento = new Orcamento();
+$orcamento->valor = 500;
+$orcamento->quantidadeItens = 7;
+
+$orcamentoExportado = new OrcamentoExportado($orcamento);
+$orcamentoExportadoZip = new ArquivoZipExportado('orcamento.array');
+
+echo $orcamentoExportadoZip->salvar($orcamentoExportado);
