5d31b4a2ba4fa2fde4d8c460aadf0f905ba60f7b
GUILHERME\Henrique <guilherme.hlg.eng@gmail.com>
2021-05-24 11:21:15 -0300

    finaliza padrao Abstract Factory.

---
+++ Design Patterns/venda.php
@@ -0,0 +1,12 @@
+<?php
+
+use Alura\DesignPattern\Venda\VendaProdutoFactory;
+use Alura\DesignPattern\Venda\VendaServicoFactory;
+
+require_once 'vendor/autoload.php';
+
+$fabricaVenda = new VendaProdutoFactory(1000);
+$venda = $fabricaVenda->criarVenda();
+$imposto = $fabricaVenda->imposto();
+
+var_dump($venda, $imposto);
