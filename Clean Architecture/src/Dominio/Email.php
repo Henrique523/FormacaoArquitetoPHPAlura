7f5b6c803173c0f0814eb6794d18cd888cf3c449
GUILHERME\Henrique <guilherme.hlg.eng@gmail.com>
2021-05-30 12:11:21 -0300

    inicia treinamento de clean architecture

---
+++ Clean Architecture/src/Dominio/Email.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Alura\Arquitetura\Dominio;
+
+class Email
+{
+  private string $endereco;
+
+  public function __construct(string $endereco)
+  {
+    if (filter_var($endereco, FILTER_VALIDATE_EMAIL) === false) {
+      throw new \InvalidArgumentException('Endereco de e-mail invalido');
+    }
+    $this->endereco = $endereco;
+  }
+
+  public function __toString()
+  {
+    return $this->endereco;
+  }
+}
